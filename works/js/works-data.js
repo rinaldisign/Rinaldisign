68cd5a5cdffedb5f2e811023bddbe07ab9c9eed4
rinaldisign <rinaldisign@gmail.com>
2026-08-21 13:00:42 +0900

    Update works-data.js

--- works/js/works-data.js
+++ works/js/works-data.js
@@ -42,7 +42,7 @@ const WORKS = [
   {
     catno: "DN.1",
     title: "Lumin Forest",
-    cover: "../img/covers/placeholder-1.jpg",
+    cover: "../img/covers/art-dn1.jpg",
     desc: "A quiet piano composition inspired by the depth of forest light. The piece moves slowly, allowing silence and resonance to create space and atmosphere. 2025",
     soundcloud: "",
     musescore: "https://musescore.com/user/118322597/scores/32785730",
@@ -53,7 +53,7 @@ const WORKS = [
   {
     catno: "DN.2",
     title: "Solastalgia",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn1.jpg",
     desc: "A reflective piece exploring emotional distance and nostalgia. Soft harmonic motion creates a fragile and contemplative musical space. 2025",
     soundcloud: "",
     musescore: "https://musescore.com/user/118322597/scores/32670671",
@@ -64,7 +64,7 @@ const WORKS = [
   {
     catno: "DN.3",
     title: "Laserila",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn1.jpg",
     desc: "I composed this piece on a cold night, alone at the piano. As I sat in the quietness of the night, a melody seemed to emerge from nothingness — unbidden, fragile, and melancholic. From that solitary moment, filled with sadness and quiet reflection, Laserila was born. 2025",
     soundcloud: "",
     musescore: "https://musescore.com/user/118322597/scores/32670398",
@@ -75,7 +75,7 @@ const WORKS = [
   {
     catno: "DN.4",
     title: "Cycle7",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn1.jpg",
     desc: "I created this simple melody in the key of C♯ minor because it is easy to play and useful for practice. It really helps train both the left and right hands. 2025",
     soundcloud: "",
     musescore: "#",
@@ -86,7 +86,7 @@ const WORKS = [
   {
     catno: "DN.5",
     title: "Furnama",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn1.jpg",
     desc: "I feel a sense of tranquility whenever I listen to this melody. Its gentle and steady flow was created as a piece for the night, evoking the quiet atmosphere of a peaceful evening beneath the moonlight. The title Furnama is inspired by the Indonesian word for 'full moon,' symbolizing the calm and serene feeling that lies at the heart of this piece. 2025",
     soundcloud: "",
     musescore: "https://musescore.com/user/118322597/scores/32670272",
@@ -97,7 +97,7 @@ const WORKS = [
     {
     catno: "DN.6",
     title: "(DATA LOST)",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn1.jpg",
     desc: "The original data was lost, along with the melody I had recorded. No trace of it remains. What survives today is only the memory that this piece once existed a small but meaningful part of my journey as a composer. 2025",
     soundcloud: "",
     musescore: "#",
@@ -108,7 +108,7 @@ const WORKS = [
    {
     catno: "DN.7",
     title: "Mycro",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn2.jpg",
     desc: "I created this piece after coming home from work feeling tired. As soon as I touched the piano keys, a lazy melody came from somewhere inside me, almost by itself. And that is how MYCRO was born. 2025",
     soundcloud: "",
     musescore: "https://musescore.com/user/118322597/scores/32671463",
@@ -119,7 +119,7 @@ const WORKS = [
    {
     catno: "DN.8",
     title: "Lucent Sepium",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn2.jpg",
     desc: "I wrote this piece while exploring a new chord progression from C# major to F minor. I only created a few very short melodic fragments in this piece. 2025",
     soundcloud: "",
     musescore: "https://musescore.com/user/118322597/scores/32783756",
@@ -130,7 +130,7 @@ const WORKS = [
   {
     catno: "DN.9",
     title: "Serein Paradisaea",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn2.jpg",
     desc: "I created this piece while exploring a chord progression in C# major. I found a simple melody that felt melancholic, matching my mood at the time I composed it. 2025",
     soundcloud: "",
     musescore: "https://musescore.com/user/118322597/scores/32813600",
@@ -141,7 +141,7 @@ const WORKS = [
   {
     catno: "DN.10",
     title: "Serene Sepium",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn2.jpg",
     desc: "I wrote this piece naturally, using the same progression as the previous work. Here, I started to feel more comfortable writing my own original music.",
     soundcloud: "",
     musescore: "https://musescore.com/user/118322597/scores/32843381",
@@ -163,7 +163,7 @@ const WORKS = [
     {
     catno: "DN.12",
     title: "Memory Glow",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn2.jpg",
     desc: "In Editing",
     soundcloud: "",
     musescore: "",
@@ -193,7 +193,7 @@ const WORKS = [
     {
     catno: "DN.15",
     title: "Velarium Harpia",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn2.jpg",
     desc: "In Editing",
     soundcloud: "",
     musescore: "",
@@ -215,7 +215,7 @@ const WORKS = [
     {
     catno: "DN.17",
     title: "Sitta Villosa",
-    cover: "../img/covers/placeholder-2.jpg",
+    cover: "../img/covers/art-dn1.jpg",
     desc: "In Editing",
     soundcloud: "",
     musescore: "",
